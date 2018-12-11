b4d11c57233774bbbb77db480e7a62bd174d20ac
rastwe <wes.rast@ey.com>
2018-12-11 11:11:01 -0500

    Starting point

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import styled from 'styled-components';
+
+const AppStyled = styled.div`
+	text-align: center;
+`;
+
+const AppHeaderStyled = styled.header`
+	background-color: #282c34;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	font-size: calc(10px + 2vmin);
+	color: white;
+`;
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<AppStyled>
+				<AppHeaderStyled>Reusable Dice</AppHeaderStyled>
+			</AppStyled>
+		);
+	}
 }
 
 export default App;
